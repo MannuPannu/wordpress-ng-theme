8d4f7f41f1a6e84b4759f8c2fef18adbd5e0d5fe
Magnus Stenqvist <magnus.p.stenqvist@gmail.com>
2016-09-11 12:50:58 +0200

    Added components

--- client/app.routing.ts
+++ client/app.routing.ts
@@ -2,6 +2,8 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { BlogComponent } from './components/blog/blog.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { GamesComponent } from './components/games/games.component';
 
 const appRoutes: Routes = [
   {
@@ -12,9 +14,17 @@ const appRoutes: Routes = [
   {
     path: 'wordpress/blog', //Same as wordpress application name :)
     component: BlogComponent
+  },
+  {
+    path: 'wordpress/games',
+    component: GamesComponent
+  },
+  {
+    path: 'wordpress/aboutme', 
+    component: AboutmeComponent
   }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
 
-export const routedComponents: any[] = [BlogComponent];
+export const routedComponents: any[] = [BlogComponent, GamesComponent, AboutmeComponent];
